750f299d789f4c9d8441f38ffda7d7e3a08da62c
jamailun <jamailun@laposte.net>
2020-05-14 11:31:56 +0200

    Monsters cannot go in vehicules

--- src/fr/jamailun/halystia/events/EntityPickupItemListener.java
+++ src/fr/jamailun/halystia/events/EntityPickupItemListener.java
@@ -1,11 +1,14 @@
 package fr.jamailun.halystia.events;
 
+import org.bukkit.ChatColor;
+import org.bukkit.Material;
 import org.bukkit.entity.Player;
 import org.bukkit.entity.Zombie;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.entity.EntityPickupItemEvent;
 import org.bukkit.event.entity.EntityTransformEvent;
 import org.bukkit.event.entity.EntityTransformEvent.TransformReason;
+import org.bukkit.event.vehicle.VehicleEnterEvent;
 
 import fr.jamailun.halystia.HalystiaRPG;
 
@@ -14,7 +17,7 @@ public class EntityPickupItemListener extends HalystiaListener {
 	public EntityPickupItemListener(HalystiaRPG main) {
 		super(main);
 	}
-	
+
 	@EventHandler
 	public void entityPickupItem(EntityPickupItemEvent e) {
 		if( ! HalystiaRPG.isInRpgWorld(e.getEntity()))
@@ -23,7 +26,7 @@ public class EntityPickupItemListener extends HalystiaListener {
 			return;
 		e.setCancelled(true);
 	}
-	
+
 	@EventHandler
 	public void transformEvent(EntityTransformEvent e) {
 		if( ! HalystiaRPG.isInRpgWorld(e.getEntity()))
@@ -34,4 +37,17 @@ public class EntityPickupItemListener extends HalystiaListener {
 		}
 	}
 
+	@EventHandler
+	public void entityRideEvent(VehicleEnterEvent e) {
+		if ( ! HalystiaRPG.isInRpgWorld(e.getEntered()) )
+			return;
+		if( ! (e.getEntered() instanceof Player) ) {
+			e.setCancelled(true);
+			return;
+		}
+		boolean valid = e.getVehicle().getLocation().getBlock().getType() == Material.WATER || e.getVehicle().getLocation().getBlock().getRelative(0, -1, 0).getType() == Material.WATER;
+		e.setCancelled(!valid);
+		if(!valid)
+			((Player)e.getEntered()).sendMessage(HalystiaRPG.PREFIX+ChatColor.RED + "Impossible d'aller sur un bateau qui n'est pas dans l'eau !");
+	}
 }
