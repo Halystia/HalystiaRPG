1fd0c139925780c767133408e0e06338ad6582ee
jamailun <jamailun@laposte.net>
2020-06-03 23:10:43 +0200

    Fixed farmalnd wich decayed

--- src/fr/jamailun/halystia/events/ConsumeItemListener.java
+++ src/fr/jamailun/halystia/events/ConsumeItemListener.java
@@ -1,9 +1,16 @@
 package fr.jamailun.halystia.events;
 
+import java.util.List;
+
 import org.bukkit.ChatColor;
+import org.bukkit.Material;
+import org.bukkit.attribute.Attribute;
 import org.bukkit.event.EventHandler;
+import org.bukkit.event.block.BlockFadeEvent;
 import org.bukkit.event.block.LeavesDecayEvent;
 import org.bukkit.event.player.PlayerItemConsumeEvent;
+import org.bukkit.inventory.ItemStack;
+import org.bukkit.inventory.meta.ItemMeta;
 
 import fr.jamailun.halystia.HalystiaRPG;
 import fr.jamailun.halystia.players.PlayerData;
@@ -18,6 +25,13 @@ public class ConsumeItemListener extends HalystiaListener {
 	public void playerConsumeItemEvent(PlayerItemConsumeEvent e) {
 		if( ! HalystiaRPG.isInRpgWorld(e.getPlayer()))
 			return;
+		
+		int health = getHealthOfItem(e.getItem());
+		if(health > -1) {
+			e.getPlayer().setHealth(Math.max(e.getPlayer().getAttribute(Attribute.GENERIC_MAX_HEALTH).getValue(), e.getPlayer().getHealth() + (health * 2)));
+			return;
+		}
+		
 		int manaGiven = main.getPotionManager().getPowerOfBottle(e.getItem());
 		if(manaGiven > -1) {
 			try {
@@ -38,5 +52,43 @@ public class ConsumeItemListener extends HalystiaListener {
 		if( ! HalystiaRPG.isRpgWorld(e.getBlock().getWorld()))
 			return;
 		e.setCancelled(true);
+	}
+	
+	
+	@EventHandler
+	public void farmDepopEvent(BlockFadeEvent e) {
+		if( ! HalystiaRPG.isRpgWorld(e.getBlock().getWorld()))
+			return;
+		
+		if(e.getBlock().getType() == Material.FARMLAND)
+			e.setCancelled(true);
+			
+	}
+	
+	private int getHealthOfItem(ItemStack item) {
+		if(item == null)
+			return -1;
+		if( ! item.hasItemMeta())
+			return -1;
+		ItemMeta meta = item.getItemMeta();
+		if( ! meta.hasLore())
+			return -1;
+		List<String> lore = meta.getLore();
+		if(lore.size() < 1)
+			return -1;
+		String line = lore.get(0);
+		if(line.startsWith(ChatColor.RED + "+")) {
+			String raw1 = line.replace(ChatColor.RED + "+", "");
+			String raw = raw1.replace("❤", "");
+			try {
+				int health = Integer.parseInt(raw);
+				return health;
+			} catch(NumberFormatException e) {
+				return -1;
+			}
+		}
+		
+		return -1;
+		
 	}
 }
