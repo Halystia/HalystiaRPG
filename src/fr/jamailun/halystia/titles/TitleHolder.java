c219de0ee5d885f851eba0cf5d8d032d2f1dd328
jamailun <jamailun@laposte.net>
2020-06-22 10:56:17 +0200

    Added an holder for health points

--- src/fr/jamailun/halystia/titles/TitleHolder.java
+++ src/fr/jamailun/halystia/titles/TitleHolder.java
@@ -18,13 +18,13 @@ public class TitleHolder extends PlaceholderExpansion {
 	private final TitlesManager titles;
 	private final DataHandler bdd;
 	private final ClasseManager players;
-	
+
 	public TitleHolder(TitlesManager titles, DataHandler bdd, ClasseManager players) {
 		this.titles = titles;
 		this.bdd = bdd;
 		this.players = players;
 	}
-	
+
 	@Override
 	public String getAuthor() {
 		return "jamailun";
@@ -39,53 +39,60 @@ public class TitleHolder extends PlaceholderExpansion {
 	public String getVersion() {
 		return "4.0.0";
 	}
-	
+
 	@Override
-    public String onPlaceholderRequest(Player p, String identifier) {
+	public String onPlaceholderRequest(Player p, String identifier) {
 
-        if ( identifier.equals("title") ) {
-        	String tag = bdd.getCurrentTitleOfPlayer(p);
-        	if(tag == null)
-        		return "";
-        	Title title = titles.getTitleWithTag(tag);
-        	if(title == null)
-        		return "";
-            return title.getDisplayName();
-        }
-        
-        if ( identifier.equals("level") ) {
-        	PlayerData pc = players.getPlayerData(p);
-        	if(pc == null)
-        		return ChatColor.GRAY + "[0] ";
-        	int level = pc.getLevel();
-        	p.setLevel(level);
-        	return (pc.getClasse() == Classe.NONE) ? GRAY+"[0] " : Classe.getColor(level)+(level>=100?BOLD+"":"")+"["+level+"] " + pc.getKarmaColor();
-        }
-        
-        if ( identifier.equals("barText") ) {
-        	PlayerData pc = players.getPlayerData(p);
-        	if(pc == null)
-        		return GRAY + "(Loading)";
-        	String text = pc.getAmeString() + " " + pc.getManaString();
-        	return text;
-        }
-        
-        if ( identifier.equals("barColor") ) {
-        	PlayerData pc = players.getPlayerData(p);
-        	if(pc == null)
-        		return BarColor.BLUE.toString();
-        	return pc.getManaBarColor().toString();
-        }
-        
-        if ( identifier.equals("barProgress") ) {
-        	PlayerData pc = players.getPlayerData(p);
-        	if(pc == null)
-        		return "100";
-        	p.setExp((float)pc.getPercentXp());
-        	return pc.getManaProgress() + "";
-        }
+		if ( identifier.equals("title") ) {
+			String tag = bdd.getCurrentTitleOfPlayer(p);
+			if(tag == null)
+				return "";
+			Title title = titles.getTitleWithTag(tag);
+			if(title == null)
+				return "";
+			return title.getDisplayName();
+		}
 
-        return "";
-    }
-	
+		if ( identifier.equals("level") ) {
+			PlayerData pc = players.getPlayerData(p);
+			if(pc == null)
+				return ChatColor.GRAY + "[0] ";
+			int level = pc.getLevel();
+			p.setLevel(level);
+			return (pc.getClasse() == Classe.NONE) ? GRAY+"[0] " : Classe.getColor(level)+(level>=100?BOLD+"":"")+"["+level+"] " + pc.getKarmaColor();
+		}
+
+		if ( identifier.equals("barText") ) {
+			PlayerData pc = players.getPlayerData(p);
+			if(pc == null)
+				return GRAY + "(Loading)";
+			String text = pc.getAmeString() + " " + pc.getManaString();
+			return text;
+		}
+		
+		if ( identifier.equals("health") ) {
+			PlayerData pc = players.getPlayerData(p);
+			if(pc == null)
+				return GRAY + "(Loading)";
+			return pc.getHealth() + "";
+		}
+
+		if ( identifier.equals("barColor") ) {
+			PlayerData pc = players.getPlayerData(p);
+			if(pc == null)
+				return BarColor.BLUE.toString();
+			return pc.getManaBarColor().toString();
+		}
+
+		if ( identifier.equals("barProgress") ) {
+			PlayerData pc = players.getPlayerData(p);
+			if(pc == null)
+				return "100";
+			p.setExp((float)pc.getPercentXp());
+			return pc.getManaProgress() + "";
+		}
+
+		return "";
+	}
+
 }
